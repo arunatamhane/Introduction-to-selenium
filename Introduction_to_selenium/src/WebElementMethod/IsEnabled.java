c0671eb5155a4a996e0732c11ad68fc8f0fee951
ADMIN <ADMIN@DESKTOP-M8PQBEB>
2023-06-16 10:33:10 +0530

    commit

--- Introduction_to_selenium/src/WebElementMethod/IsEnabled.java
+++ Introduction_to_selenium/src/WebElementMethod/IsEnabled.java
@@ -2,14 +2,17 @@ package WebElementMethod;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class IsEnabled {
 	public static void main(String[] args) {
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://demo.actitime.com/login.do");
-		WebDriver button=driver.findElement(By.id("loginButtonContainer"));
+		driver.get("https://www.instagram.com");
+		WebElement login=driver.findElement(By.xpath("//button[.='Log in']"));
+		System.out.println(login.isEnabled());
+		
 		
 	}
 
